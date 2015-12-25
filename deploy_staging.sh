04c1dee06aca749f9f8aa8ad6d7ff0fb7328d251
Randy Burgess <wrburgess@gmail.com>
2015-12-25 14:48:48 -0600

    circle.yml setup

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-DEFAULT="allaboardapps"
+DEFAULT="default"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=allaboardapps-site-staging
-DIR=dist/
+DIR=/
 aws s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE"
