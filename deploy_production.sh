01eff3b4fa807fb52bd5dbfd1b976f1a2fdceac3
Randy Burgess <wrburgess@gmail.com>
2015-12-26 00:40:04 -0600

    trying more awlci stuff with circleci

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,5 +1,5 @@
-DEFAULT="default"
-PROFILE=${AWS_PROFILE:-$DEFAULT}
+# DEFAULT="default"
+# PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=allaboardapps-site-production
 DIR=./dist
 aws s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE"
--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,5 +1,5 @@
-DEFAULT="default"
-PROFILE=${AWS_PROFILE:-$DEFAULT}
+# DEFAULT="default"
+# PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=allaboardapps-site-staging
 DIR=./dist
-aws s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE"
+aws s3 sync $DIR s3://$BUCKET/
