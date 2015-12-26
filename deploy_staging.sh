6ec0d8446d471ed3db7b033efe040c245b22464f
Randy Burgess <wrburgess@gmail.com>
2015-12-26 11:21:23 -0600

    finally got deployment working

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-gulp build
-gulp dist
 DEFAULT="default"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=allaboardapps-site-staging
 DIR=./dist
-aws s3 sync $DIR s3://$BUCKET/
+aws s3 sync $DIR s3://$BUCKET/ --delete
