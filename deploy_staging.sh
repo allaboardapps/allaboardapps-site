d24ee7b9cd7defa19049385bcd912bb93ab216ac
Randy Burgess <wrburgess@gmail.com>
2015-12-26 11:07:24 -0600

    trying more awlci stuff with circleci

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,5 +1,7 @@
-# DEFAULT="default"
-# PROFILE=${AWS_PROFILE:-$DEFAULT}
+gulp build
+gulp dist
+DEFAULT="default"
+PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=allaboardapps-site-staging
 DIR=./dist
 aws s3 sync $DIR s3://$BUCKET/
