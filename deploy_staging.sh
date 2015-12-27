781b5617a79d565c5434f45cb726362a175cd2ac
Randy Burgess <wrburgess@gmail.com>
2015-12-27 15:34:36 -0600

    adding cloudfront cache kill

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -3,7 +3,7 @@ DEFAULT="default"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=allaboardapps-site-staging
 DIR=./dist
-DISTRIBUTION_ID=E3FF3J1WDZ180X
+DISTRIBUTION=E3FF3J1WDZ180X
 aws s3 sync $DIR s3://$BUCKET/ --delete
 aws configure set preview.cloudfront true
-aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /index.html
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION --paths /index.html
