e91dc9ebe4079387cdfab1ce661b15ed1373059c
Randy Burgess <wrburgess@gmail.com>
2015-12-27 13:59:12 -0600

    attempting to use gulp

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -6,8 +6,8 @@ var App = React.createClass({
   render() {
     return (
       <div className="app">
-        <h1>All Aboard Apps, LLC</h1>
-        Randy Burgess, Dev<br/>
+        <h1>All Aboard Apps</h1>
+        Randy Burgess, Developer<br/>
         773-551-6808<br/>
         <a href="mailto:randy@allaboardapps.com">randy@allaboardapps.com</a><br/>
         <img src="apple-icon.png" />
