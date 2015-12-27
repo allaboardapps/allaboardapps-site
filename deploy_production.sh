d002dd08b9dadce9e8d89e1ea0613e724e866316
Randy Burgess <wrburgess@gmail.com>
2015-12-27 15:48:41 -0600

    trying production deploy

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 DEFAULT="default"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
-BUCKET=allaboardapps-site-staging
+BUCKET=allaboardapps-site-production
 DIR=./dist
 DISTRIBUTION_ID=E2KTK9BDAJSU3C
+INVALIDATIONS=invalidations.json
 aws s3 sync $DIR s3://$BUCKET/ --delete
 aws configure set preview.cloudfront true
-aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths /index.html
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION --invalidation-batch file://$INVALIDATIONS
 
