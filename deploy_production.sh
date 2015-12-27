961567e81d7ba1d8c86925fea8a2230b5203ab3c
Randy Burgess <wrburgess@gmail.com>
2015-12-27 15:50:33 -0600

    trying production deploy

--- deploy_production.sh
+++ deploy_production.sh
@@ -3,7 +3,7 @@ DEFAULT="default"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=allaboardapps-site-production
 DIR=./dist
-DISTRIBUTION_ID=E2KTK9BDAJSU3C
+DISTRIBUTION=E2KTK9BDAJSU3C
 INVALIDATIONS=invalidations.json
 aws s3 sync $DIR s3://$BUCKET/ --delete
 aws configure set preview.cloudfront true
