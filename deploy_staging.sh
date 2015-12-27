54f6441da99f8e9bad8ecc78f9a5a1032baf8ce4
Randy Burgess <wrburgess@gmail.com>
2015-12-27 15:07:50 -0600

    adding cloudfront cache invalidation

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -4,3 +4,4 @@ PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=allaboardapps-site-staging
 DIR=./dist
 aws s3 sync $DIR s3://$BUCKET/ --delete
+aws cloudfront create-invalidation --distribution-id E1PCO6MVHOZB3A --paths /index.html
