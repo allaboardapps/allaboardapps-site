b7972b6fc28b16c7905ab336dcb566e7e3cc39b3
Randy Burgess <wrburgess@gmail.com>
2016-01-17 17:27:17 -0600

    adding new logo

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -6,11 +6,12 @@ var App = React.createClass({
   render() {
     return (
       <div className="app">
-        <h1>All Aboard Apps</h1>
-        Randy Burgess, Developer<br/>
-        773-551-6808<br/>
-        <a href="mailto:randy@allaboardapps.com">randy@allaboardapps.com</a><br/>
-        <img className="what" src="apple-icon.png" />
+        <img className="company-logo" src="all-aboard-apps-logo.jpg" alt="All Aboard Apps, LLC" />
+        <div className="contact-info">
+          Randy Burgess, Developer<br/>
+          773-551-6808<br/>
+          <a href="mailto:randy@allaboardapps.com">randy@allaboardapps.com</a><br/>
+        </div>
       </div>
     )
   }
